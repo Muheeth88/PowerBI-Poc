1ef1cdb99c103fc5a155257993aa2f1454205da3
Abdul Muheeth <abdulmuheeth88@gmail.com>
2024-03-08 01:53:19 +0530

    rendered

--- src/pages/dashboards/Dashboard.jsx
+++ src/pages/dashboards/Dashboard.jsx
@@ -4,43 +4,100 @@ import { models } from 'powerbi-client';
 import { PowerBIEmbed } from 'powerbi-client-react';
 import axios from 'axios';
 import { BEARER_TOKEN } from '../../utils/constants';
+import { useParams } from 'react-router-dom';
+import "../dashboards/Dashboard.css"
 
 const Dashboard = () => {
+  const { reportname } = useParams();
+  const [reportId, setReportId] = useState("")
+  const [datasetId, setDatasetId] = useState("")
+  const [embedUrl, setEmbedUrl] = useState("")
+  const [accessToken, setAccessToken] = useState("")
+  const [pbiConfigured, setPbiConfigured] = useState(false)
 
-  const [reportConfig, setReportConfig] = useState({
-    type: 'report',
-    embedUrl: undefined,
-    accessToken: undefined,
-    id: undefined,
-    tokenType: models.TokenType.Embed,
-    settings: {
-      panes: {
-        filters: {
-          expanded: false,
-          visible: true
-        }
-      },
-      background: models.BackgroundType.Transparent,
-    }
-  });
+  const [reportConfig, setReportConfig] = useState({});
 
   useEffect(() => {
-    setReportConfig(prevState => ({
-      ...prevState,
-      embedUrl: "x",
-      accessToken: "y",
-      id: "z"
-    }));
-    console.log(reportConfig)
+    getReportDetails();
+    getAccessToken();
   }, []);
+
+  const getReportDetails = () => {
+    setDatasetId(localStorage.getItem("datasetid"))
+    setReportId(localStorage.getItem("reportid"))
+    setEmbedUrl(localStorage.getItem("embedurl"))
+
+    getAccessToken();
+  }
+
+  const getAccessToken = async() => {
+   
+    let payload = {
+      datasets: [
+        {
+          id : localStorage.getItem("datasetid")
+        }
+      ],
+      reports: [
+        {
+          id: localStorage.getItem("reportid")
+        }
+      ]
+    }
+
+    try {
+      const response = await axios.post('https://api.powerbi.com/v1.0/myorg/GenerateToken', payload, {
+        headers: {
+          Authorization: `Bearer ${BEARER_TOKEN}`,
+        },
+      });
+      console.log(response.data.token)
+      setAccessToken(response.data.token)
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+    // setTimeout( updateReportConfig(), 2000)
+    updateReportConfig();
+  }
+
+  const updateReportConfig = () => {
+    setReportConfig({
+      type: 'report',
+      embedUrl: embedUrl,
+      accessToken: accessToken,
+      id: reportId,
+      tokenType: models.TokenType.Embed,
+      settings: {
+        panes: {
+          filters: {
+            expanded: false,
+            visible: true
+          }
+        },
+      }
+    })
+    setPbiConfigured(true)
+  //  setTimeout( setPbiConfigured(true), 2000)
+  }
 
   return (
   <> 
-    <div>
-        <PowerBIEmbed
+    <div className='wrapper'>
+      <div className='mb-2 flex justify-center'>{reportname}</div>
+      {pbiConfigured && <PowerBIEmbed
           embedConfig = {reportConfig}
           cssClassName = 'power-bi-report-class'
-        />
+          eventHandlers={
+            new Map([
+              ['loaded', function () { console.log('Report loaded'); }],
+              ['rendered', function () { console.log('Report rendered'); }],
+              ['error', function (event) { console.log(event.detail); }]
+            ])
+          }
+          getEmbeddedComponent={(embeddedReport) => {
+            window.report = embeddedReport;
+          }}
+        /> } 
     </div>
   </>
   )
